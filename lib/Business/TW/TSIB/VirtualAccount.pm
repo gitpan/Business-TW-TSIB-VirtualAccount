a36fa15ecaea07bad7d8d8ced42ab8b6bc886d2b
Yo-An Lin (林佑安) <cornelius@cpan.org>
2007-10-26 06:54:48 +0000

    Import of CORNELIUS/Business-TW-TSIB-VirtualAccount-0.02 from CPAN.
    gitpan-cpan-distribution: Business-TW-TSIB-VirtualAccount
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CORNELIUS/Business-TW-TSIB-VirtualAccount-0.02.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

--- lib/Business/TW/TSIB/VirtualAccount.pm
+++ lib/Business/TW/TSIB/VirtualAccount.pm
@@ -10,11 +10,11 @@ Business::TW::TSIB::VirtualAccount - Module for Taishin Bank Virtual Account Man
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
